2b132a7d4b99dc55c19a47c3508d8d4a6679d96a
anupamae <anupamae8@gmail.com>
2024-02-06 06:11:03 +0100

    brought all templates into home.html and didn't change template inheritance given for navbar

--- app_portfolio/urls.py
+++ app_portfolio/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from .views import home, download_pdf, about, experience, skills, education, contacts, projects
 from django.conf import settings
+from django.urls import path
+from .views import home, download_pdf
 from django.conf.urls.static import static
 
 app_name = 'app_portfolio'
@@ -8,12 +8,6 @@ app_name = 'app_portfolio'
 urlpatterns = [
     path('', home, name='home'),
     path('download-pdf/', download_pdf, name='download_pdf'),
-    path('about/', about, name='about'),
-    path('skills/', skills, name='skills'),
-    path('experience/', experience, name='experience'),
-    path('education/', education, name='education'),
-    path('projects/', projects, name='projects'),
-    path('contacts/', contacts, name='contacts')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app_portfolio/views.py
+++ app_portfolio/views.py
@@ -1,62 +1,32 @@
 from django.shortcuts import render, HttpResponse
-from .models import Home, About, Experience, Projects, Skills, Education, Contacts
+from .models import Home, About, Experience, Projects, Skills, Education
 
 # Create your views here.
+
+
 def home(request):
     homes = Home.objects.all()
+    about_data = About.objects.all()
+    experience_data = Experience.objects.all()
+    skills_data = Skills.objects.all()
+    projects_data = Projects.objects.all()
+    education_data = Education.objects.all()
     context = {
-        "homes": homes
+        "homes": homes,
+        "about_data": about_data,
+        "experience_data": experience_data,
+        "skills_data": skills_data,
+        "projects_data": projects_data,
+        "education_data": education_data
     }
-    return render(request, 'app_portfolio/home.html', context) 
-def download_pdf(request):
-    home_instance = Home.objects.first()  # Assuming you want to download from the first instance
-    pdf_path = home_instance.resume.path  # Assuming your model has a FileField named 'pdf_file'
+    return render(request, 'app_portfolio/home.html', context)
 
-    # Open the PDF file
+
+def download_pdf(request):
+    home_instance = Home.objects.first()
+    pdf_path = home_instance.resume.path
+
     with open(pdf_path, 'rb') as resume:
         response = HttpResponse(resume.read(), content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="example.pdf"'
         return response
-
-def about(request):
-    about_data = About.objects.all()  
-    context = {
-        "about_data": about_data
-    }
-    return render(request, 'app_portfolio/about.html', context=context)
-
-def experience(request):
-    experience_data = Experience.objects.all()
-    context = {
-        "experience_data":experience_data
-    }
-    return render(request, 'app_portfolio/experience.html', context)
-
-def skills(request):
-    skills_data = Skills.objects.all()
-    context = {
-        "skills_data": skills_data
-    }
-    return render(request, 'app_portfolio/skills.html', context)
-
-def education(request):
-    education_data = Education.objects.all()
-    context = {
-        "education_data": education_data
-    }
-    return render(request, 'app_portfolio/education.html', context)
-
-def projects(request):
-    projects_data = Projects.objects.all()
-    context = {
-        "projects_data": projects_data
-    }
-    return render(request, 'app_portfolio/projects.html', context)
-
-
-def contacts(request):
-    contacts_data = Contacts.objects.all()
-    context = {
-        "contacts_data": contacts_data
-    }
-    return render(request, 'app_portfolio/contacts.html', context)
